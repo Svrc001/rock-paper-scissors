4a95f638796dd9c060bdc42e45ac5ac019bf59ea
Srividya R. Chitrao <srchitrao@gmail.com>
2026-02-03 23:06:07 +0530

    Add JS function to get computer choice
    The changes include:
    - An HTML file linking JavaScript file
    - Implementing a method that generates a random input among 3 choices viz., "rock", "paper", "scissors"

---
+++ script.js
@@ -0,0 +1,20 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+
+    //Generate random number between 0 and 2
+    let random_number = Math.floor(Math.random() * 3);
+
+    switch(random_number) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+
+}
+
+
