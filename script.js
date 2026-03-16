61340c73b4684cab3a18bc455f07c887f142f708
Srividya R. Chitrao <srchitrao@gmail.com>
2026-03-16 23:00:00 +0530

    Add JS event listeners based on user clicks
    The changes include:
    - Displaying running score
    - Displaying and removing the end result based on winning score

--- script.js
+++ script.js
@@ -14,55 +14,81 @@ function getComputerChoice() {
         case 2:
             return "scissors";
     }
-
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("Choose rock, paper or scissors!");
-     
-    return humanChoice.toLowerCase();
-}
+
 
 function playRound(humanChoice, computerChoice) {
 
+    const para = document.querySelector('.running-score');     
+
     if(humanChoice === computerChoice) {
-        console.log(`Its a tie! ${humanChoice} and ${computerChoice}`);
+        para.textContent = `You: ${humanScore} Computer: ${computerScore}. Its a tie! ${humanChoice} and ${computerChoice}`;
     }
 
     else if((humanChoice === "rock" && computerChoice === "scissors")
             || (humanChoice === "paper" && computerChoice === "rock") 
             || (humanChoice === "scissors" && computerChoice === "paper")) {
 
-        console.log(`You won! ${humanChoice} beats ${computerChoice}`);
         humanScore++;
+        para.textContent = `You: ${humanScore} Computer: ${computerScore}. You won! ${humanChoice} beats ${computerChoice}`;
     } 
 
     else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
         computerScore++;
+        para.textContent = `You: ${humanScore} Computer: ${computerScore}. You lose! ${computerChoice} beats ${humanChoice}`;
+    }
+}
+
+function play(event) {
+    let humanChoice = event.target.id;
+    let computerChoice = getComputerChoice();
+
+    const winner = document.querySelector('.winner');
+    if(winner) {
+        winner.remove();
+    }
+
+    playRound(humanChoice, computerChoice);
+
+    if(humanScore === 5 || computerScore === 5) {
+        getWinner();
+
+        //reset the score
+        humanScore = 0;
+        computerScore = 0;
+
     }
 
 }
 
-function playGame() {
-
-    for(let i=0; i<5; i++) {
-
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-        
-        playRound(humanChoice, computerChoice);
-    }
+function getWinner() {
+    const resultsDiv = document.querySelector('.results');
+    const winner = document.createElement('h3');
+    winner.setAttribute('class', 'winner');
 
     if(humanScore > computerScore) {
-        console.log(`You won against the Computer with scores ${humanScore} and ${computerScore} respectively`);
+        winner.textContent = `You won against the Computer with scores ${humanScore} and ${computerScore} respectively`;
     }
     else if(humanScore < computerScore) {
-        console.log(`You lost against the Computer with scores ${humanScore} and ${computerScore} respectively`);
+        winner.textContent = `You lost against the Computer with scores ${humanScore} and ${computerScore} respectively`;
     }
     else {
-        console.log(`Its a tie with scores ${humanScore} and ${computerScore} respectively`);
+        winner.textContent = `Its a tie with scores ${humanScore} and ${computerScore} respectively`;
     }
+
+    resultsDiv.appendChild(winner);    
+}
+
+function playGame() {
+   
+    let rock = document.querySelector('#rock');
+    let paper = document.querySelector('#paper');
+    let scissors = document.querySelector('#scissors');
+
+    rock.addEventListener('click', play);
+    paper.addEventListener('click', play);
+    scissors.addEventListener('click', play);   
 
 }
 
